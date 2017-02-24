afeed16ab6cbfd3442f243655cd123efea5bd1cd
Minh Phuong <mphuong@kent.edu>
2017-02-24 08:54:44 -0800

    add on constructor for client

---
+++ Objects/Client.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  public class Client
+  {
+    private int _id;
+    private string _name;
+    private string _request;
+    private DateTime _date;
+    private int _stylistId;
+
+  }
+}
