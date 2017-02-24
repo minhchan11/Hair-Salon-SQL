19ec96a35731c644820997cba2b883a23be4729e
Minh Phuong <mphuong@kent.edu>
2017-02-24 08:59:15 -0800

    add public getters and setters for clients

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -21,5 +21,52 @@ namespace HairSalon
       _stylistId = StylistId;
     }
 
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public int SetId(int newId)
+    {
+      _id = newId;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetRequest()
+    {
+      return _request;
+    }
+    public void SetRequest(string newRequest)
+    {
+      _request = newRequest;
+    }
+
+    public DateTime GetDate()
+    {
+      return _date;
+    }
+    public void SetDate(DateTime newDate)
+    {
+      _date = newDate;
+    }
+
+    public int GetStylistId()
+    {
+      return _stylistId;
+    }
+    public void SetStylistId(int newStylistId)
+    {
+      _stylistId = newStylistId;
+    }
+
+
   }
 }
